9a955f3b76b67e2cdb63918c03262535c799dccf
dfjung4254 <rmsghk4254@naver.com>
2019-07-30 22:01:17 +0900

    test

--- routes/login.js
+++ routes/login.js
@@ -36,7 +36,7 @@ router.post('/', function(req, res) {
 
     */
  
-    res.write(googleToken); //OK라는 내용이 안드로이드의 ReadBuffer를 통해 result String으로 바뀜
+    res.write(googleToken.toString()); //OK라는 내용이 안드로이드의 ReadBuffer를 통해 result String으로 바뀜
     res.end();
 
 });
