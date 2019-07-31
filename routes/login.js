8a1a00d5ccb54abc902f8d6cb220402c152821d7
dfjung4254 <rmsghk4254@naver.com>
2019-07-31 20:14:01 +0900

    logintest

--- routes/login.js
+++ routes/login.js
@@ -17,29 +17,19 @@ router.post('/', function(req, res) {
     */
 
     /* 구글 토큰을 담는다. */
-    var googleToken = "";
+    var googleToken = req.body.id_token;
 
-    /* data 를 googleToken에 넣음 */
-    // req.on('data',function(data){
-    //     googleToken += data;
-    // });
-
-    googleToken = req.body.id_token;
-
- 
-    //더 이상 데이터가 들어오지 않는다면 end 이벤트 실행
-    req.on('end',function(){
-        console.log("token : "+googleToken);
-        res.writeHead(200);
-    });
+    
 
     /*
 
         이 사이에 res를 보내기 전에 절차를 행한다.
 
     */
- 
-    res.write(googleToken); //OK라는 내용이 안드로이드의 ReadBuffer를 통해 result String으로 바뀜
+
+    /* 응답 설정 */
+    res.writeHead(200);
+    res.write(googleToken);
     res.end();
 
 });
