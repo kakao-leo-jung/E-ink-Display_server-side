99e8789d16837246fe6e31f200e586354db2ebf9
dfjung4254 <rmsghk4254@naver.com>
2019-08-03 20:23:52 +0900

    dbTeeest

--- bin/dbConnect.js
+++ bin/dbConnect.js
@@ -5,7 +5,7 @@ var mongoose = require('mongoose');
     MongoDB 에 연결해주는 커스텀 DB 커넥션 모듈
 
 */
-function connectLocalDB(dbName) {
+var dbConnect = function connectLocalDB(dbName) {
     mongoose.Promise = global.Promise;
     var db = mongoose.connection;
     db.on('error', console.error);
