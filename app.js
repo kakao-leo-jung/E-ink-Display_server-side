68849446b44e628979289a0d3e031c0a4b44fab6
dfjung4254 <rmsghk4254@naver.com>
2019-08-06 23:10:16 +0900

    json

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json());
+//app.use(express.json());
 
 /*
 
