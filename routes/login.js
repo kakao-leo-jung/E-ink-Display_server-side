3213c97f9936bd2ba7d2b33c6d9ad6de4a60a42d
dfjung4254 <rmsghk4254@naver.com>
2019-08-11 02:56:53 +0900

    authCodeTest

--- routes/login.js
+++ routes/login.js
@@ -67,7 +67,7 @@ router.post('/', function (req, res) {
 async function returnJWT(authCode, res) {
 
     /* authCode 로 부터 토큰을 추출해 낸다. */
-    const {tokens} = await client.getToken(authCode);
+    //const {tokens} = await client.getToken(authCode);
 
     console.log("getToken** : " + tokens);
 
