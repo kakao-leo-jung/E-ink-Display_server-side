faa42eabfd5369e5735e5f663f4a28083df48d78
dfjung4254 <rmsghk4254@naver.com>
2019-08-11 22:44:34 +0900

    calendarTest

--- routes/calendar.js
+++ routes/calendar.js
@@ -129,7 +129,7 @@ async function getToken(user_id) {
 
     console.log("resultUser.access_token : " + resultUser.access_token);
 
-    if (!resultUser) {
+    if (!resultUser.access_token) {
         console.log("return resultUser.access_token");
         return resultUser.access_token;
     } else {
