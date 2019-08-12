bf2446447dda8f02a8994e5b581aa0c4dd2648d8
dfjung4254 <rmsghk4254@naver.com>
2019-08-12 22:19:22 +0900

    calendarTest

--- routes/login.js
+++ routes/login.js
@@ -86,6 +86,10 @@ async function returnJWT(authCode, res) {
     console.log("getToken Method Result [tokens] : " + tokens);
     oAuth2Client.setCredentials(tokens);
 
+    console.log("::getToken analysis ACCESS : " + tokens.access_token);
+    console.log("::getToken analysis REFRESH : " + tokens.refresh_token);
+    console.log("::getToken analysis ID : " + tokens.id_token);
+
     /*
     
         accessToken 의 만료시점이 다가올 경우 감지하여 refreshToken 을 발급받는다.
